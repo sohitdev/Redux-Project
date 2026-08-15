6a1f75f83236aa55261a6bd40feffce61e358c9b
sohit kumar <sohitkumar9548@gmail.com>
2026-08-15 21:59:43 +0530

    implemented search slice

---
+++ src/redux/features/collectionSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const collectionSlice = createSlice({
+  name: "collection",
+  initialState: {},
+  reducers: {},
+});
+
+export default collectionSlice.reducer;
